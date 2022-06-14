9b9ac1c52d574dbe90a818977cf88e288a7ff46b
Cesar Cornejo <cadalcito@gmail.com>
2022-06-13 19:27:26 -0500

    Add alt to imgs

--- src/components/home/Banners.jsx
+++ src/components/home/Banners.jsx
@@ -2,10 +2,10 @@ export default function Banner({ banners }) {
     return (
       <div className="Banner">
         <div className="slider">
-            {banners && banners.map(({ data: { title, main_image: { url } }}) => (
+            {banners && banners.map(({ data: { title, main_image: { url, alt } }}) => (
                 <div className="banner-info">
                     <div className="banner-details">{title}</div>
-                    <img src={url} />
+                    <img src={url} alt={alt}/>
                 </div>
             ))}
         </div>
--- src/components/home/Products.jsx
+++ src/components/home/Products.jsx
@@ -4,9 +4,9 @@ export default function Products({ products }) {
       <h2 className="Products-title">Products</h2>
       <div className="Products-container">
       {products && products.map(
-        ({ data: { name, price, category: { slug }, mainimage: { url } }}) => (
+        ({ data: { name, price, category: { slug }, mainimage: { url, alt } }}) => (
         <div className="products-info">
-          <img src={url} />
+          <img src={url} alt={alt} />
           <div className="products-details">
             <div class="products-details__name">{name}</div>
             <div class="products-details__price">
