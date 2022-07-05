84a8b5eda64b5b3597ae0497fdc730049cdf3923
Cesar Cornejo <cadalcito@gmail.com>
2022-07-04 23:41:09 -0500

    Add deliverable 4

--- src/components/home/Products.jsx
+++ src/components/home/Products.jsx
@@ -1,11 +1,15 @@
 import { Link, useNavigate } from 'react-router-dom';
 import "./Products.scss";
 
+import { useDispatch } from "react-redux";
+import { add } from '../../redux/slices/cartSlice';
+
 export default function Products(
   { products, showAllProducts = false, next, prev, handleNext, handlePrev }
 ) {
   let navigate = useNavigate();
   const hasProducts = products && products.length > 0;
+  let dispatch = useDispatch();
 
   return (
     <div className="Products">
@@ -15,34 +19,36 @@ export default function Products(
       )}
       <div className="Products-container">
       {hasProducts && products.map(
-        ({ id, data: { name, price, category: { slug }, mainimage: { url, alt } }}) => (
-          <div
-            className="products-controller"
-            key={id}
-            onClick={() => {
-              console.log("Id", id);
-              navigate(`/product/${id}`)
+        (product) => {
+          const { id, data: { name, price, category: { slug }, mainimage: { url, alt }}} = product;
+          return (
+            <div
+              className="products-controller"
+              key={id}
+              onClick={() => {
+                console.log("Id", id);
+                navigate(`/product/${id}`)
+              }
             }
-          }
-          >
-            <div className="products-info">
-              <img src={url} alt={alt} />
-              <div className="products-details">
-                <div className="products-details__name">{name}</div>
-                <div className="products-details__price">
-                  <div className="products-details__price-category">{slug}</div>
-                  <div>${price}</div>
+            >
+              <div className="products-info">
+                <img src={url} alt={alt} />
+                <div className="products-details">
+                  <div className="products-details__name">{name}</div>
+                  <div className="products-details__price">
+                    <div className="products-details__price-category">{slug}</div>
+                    <div>${price}</div>
+                  </div>
                 </div>
               </div>
+              <button
+                className="products-add-cart"
+                onClick={(e) => { e.stopPropagation(); dispatch(add({ product, amount: 1 })); }}
+              >
+                Add to Cart
+              </button>
             </div>
-            <button
-              className="products-add-cart"
-              onClick={(e) => { e.stopPropagation(); console.log("AddCart ", id); }}
-            >
-              Add to Cart
-            </button>
-          </div>
-      ))}
+        )})}
       </div>
       {
         showAllProducts && <div className="products-link">
